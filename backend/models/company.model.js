72fd120d2e98743587bfe1c3336307ee4ebc1f3e
Vidushi Tiwari <128062856+vidsht@users.noreply.github.com>
2024-12-21 21:06:21 +0530

    added schemas

---
+++ backend/models/company.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:true,
+    },
+    description: {
+        type:String,
+    },
+    Website: {
+        type:String,
+    },
+    location: {
+        type:String,
+    },
+    logo: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    }
+},{timestamps:true})
+
+export const Company = mongoose.model("Company", companySchema);
