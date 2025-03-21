0805c3ea5a0a21ed01a5dabe716ff96552e877bb
Vidushi Tiwari <128062856+vidsht@users.noreply.github.com>
2025-03-21 13:41:18 +0530

    Depolyment issue resolved.

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
-    origin:'https://jobhunter-8l3n.onrender.com/login',
+    origin:'https://jobhunter-8l3n.onrender.com',
     credentials: true
     // allowedHeaders: ['Authorization', 'Content-Type'] 
 }
